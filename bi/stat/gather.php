2659d86018e6beb7cc9f3912ddfab8d0e314534f
dujiancn <568594152@qq.com>
2016-07-11 09:31:14 +0800

    modify gather.php

--- bi/stat/gather.php
+++ bi/stat/gather.php
@@ -11,17 +11,19 @@
     $ageArr = array();//age range 0:<=18, 1:19~28, 2:29~38, 3:39~48, 4:49~58, 5:>59, unknown 
     $genderArr = array();  
     $mixArr = array();
+    $intersectionArr = array();
     $nationFile = $dirPath."nation.txt";
     $ageFile = $dirPath."age.txt";
     $genderFile = $dirPath."gender.txt";
     $mixFile = $dirPath."mix.txt";
-    system("rm {$nationFile}");system("rm {$ageFile}");system("rm {$genderFile}");system("rm {$mixFile}");
+    $intersectionFile = $dirPath."intersection.txt";
+    system("rm {$nationFile}");system("rm {$ageFile}");system("rm {$genderFile}");system("rm {$mixFile}");system("rm {$intersectionFile}");
     foreach($originArr as $originLine){
         $array = explode("|",$originLine); 
         foreach($array as &$one){
             $one = trim($one);
         }
-        $price = $array[1]; 
+        $consumption = $array[1]; 
         $age = !empty($array[2]) ? $array[2] : "unknown";
         if($age != "unknown"){
             if($age<=18){
@@ -43,64 +45,79 @@
         //国家级别的统计
         if(isset($nationArr[$nation])){
             $nationArr[$nation]["num"] +=1;
-            $nationArr[$nation]["price"] +=$price;
+            $nationArr[$nation]["consumption"] +=$consumption;
         }else{
             $nationArr[$nation]["nation"] = $nation;
             $nationArr[$nation]["num"] =1;
-            $nationArr[$nation]["price"] =$price;
+            $nationArr[$nation]["consumption"] =$consumption;
         } 
         //年龄级别的统计
         if(isset($ageArr[$age])){
             $ageArr[$age]["num"] +=1;
-            $ageArr[$age]["price"] +=$price;
+            $ageArr[$age]["consumption"] +=$consumption;
         }else{
             $ageArr[$age]["age"] = $age;
             $ageArr[$age]["num"] =1;
-            $ageArr[$age]["price"] =$price;
+            $ageArr[$age]["consumption"] =$consumption;
         } 
         //性别级别的统计
         if(isset($genderArr[$gender])){
             $genderArr[$gender]["num"] +=1;
-            $genderArr[$gender]["price"] +=$price;
+            $genderArr[$gender]["consumption"] +=$consumption;
         }else{
             $genderArr[$gender]["gender"] =$gender;
             $genderArr[$gender]["num"] =1;
-            $genderArr[$gender]["price"] =$price;
+            $genderArr[$gender]["consumption"] =$consumption;
         }
 
         //综合数据的统计
         if(isset($mixArr[$nation]["age"][$age])){
             $mixArr[$nation]["age"][$age]["num"] += 1;
-            $mixArr[$nation]["age"][$age]["price"] += $price;
+            $mixArr[$nation]["age"][$age]["consumption"] += $consumption;
         }else{
             $mixArr[$nation]["age"][$age]["age"] = $age;
             $mixArr[$nation]["age"][$age]["num"] = 1;
-            $mixArr[$nation]["age"][$age]["price"] = $price;
+            $mixArr[$nation]["age"][$age]["consumption"] = $consumption;
         }
         if(isset($mixArr[$nation]["gender"][$gender])){
             $mixArr[$nation]["gender"][$gender]["num"] += 1;
-            $mixArr[$nation]["gender"][$gender]["price"] += $price;
+            $mixArr[$nation]["gender"][$gender]["consumption"] += $consumption;
         }else{
             $mixArr[$nation]["gender"][$gender]["gender"] = $gender;
             $mixArr[$nation]["gender"][$gender]["num"] = 1;
-            $mixArr[$nation]["gender"][$gender]["price"] = $price;
+            $mixArr[$nation]["gender"][$gender]["consumption"] = $consumption;
         }
-         
+        
+        //国籍*性别*年龄 交集     
+        if(isset($intersectionArr[$nation][$gender][$age])){
+            $intersectionArr[$nation][$gender][$age]["num"] += 1;
+            $intersectionArr[$nation][$gender][$age]["consumption"] += $consumption;
+        }else{
+            $intersectionArr[$nation][$gender][$age]["age"] = $age;
+            $intersectionArr[$nation][$gender][$age]["num"] = 1;
+            $intersectionArr[$nation][$gender][$age]["consumption"] = $consumption;
+        }
     }
     //写入文件
+    $line = "nation\tnum\tconsumption\n";
+    file_put_contents($nationFile,$line,FILE_APPEND);
     foreach($nationArr as $one){
-        $one["price"] = round($one["price"]/$one["num"],2);
-        $line = implode("\t\t",$one)."\n";
+        $one["consumption"] = round($one["consumption"]/$one["num"],2);
+        $line = implode("\t",$one)."\n";
         file_put_contents($nationFile,$line,FILE_APPEND);
     }  
+    $line = "age\tnum\tconsumption\n";
+    file_put_contents($ageFile,$line,FILE_APPEND);
     foreach($ageArr as $one){
-        $one["price"] = round($one["price"]/$one["num"],2);
-        $line = implode("\t\t",$one)."\n";
+        $one["consumption"] = round($one["consumption"]/$one["num"],2);
+        $line = implode("\t",$one)."\n";
         file_put_contents($ageFile,$line,FILE_APPEND);
     }  
+    $line = "gender\tnum\tconsumption\n";
+    file_put_contents($genderFile,$line,FILE_APPEND);
     foreach($genderArr as $one){
-        $one["price"] = round($one["price"]/$one["num"],2);
-        $line = implode("\t\t",$one)."\n";
+        $one["consumption"] = round($one["consumption"]/$one["num"],2);
+        $line = implode("\t",$one)."\n";
         file_put_contents($genderFile,$line,FILE_APPEND);
     }
     foreach($mixArr as $nation => $mix){
@@ -108,20 +125,43 @@
         file_put_contents($mixFile,$line,FILE_APPEND);
         $mixAgeArr = $mix["age"]; 
         $mixGenderArr = $mix["gender"]; 
-        $line = "(age)\n";
+        $line = "age\tnum\tconsumption\n";
         file_put_contents($mixFile,$line,FILE_APPEND);
         foreach($mixAgeArr as $age => $one){
-            $one["price"] = round($one["price"]/$one["num"],2);
-            $line = implode("\t\t",$one)."\n";
+            $one["consumption"] = round($one["consumption"]/$one["num"],2);
+            $line = implode("\t",$one)."\n";
             file_put_contents($mixFile,$line,FILE_APPEND);
         }
-        $line = "(gender)\n";
+        $line = "gender\tnum\tconsumption\n";
         file_put_contents($mixFile,$line,FILE_APPEND);
         foreach($mixGenderArr as $gender => $one){
-            $one["price"] = round($one["price"]/$one["num"],2);
-            $line = implode("\t\t",$one)."\n";
+            $one["consumption"] = round($one["consumption"]/$one["num"],2);
+            $line = implode("\t",$one)."\n";
             file_put_contents($mixFile,$line,FILE_APPEND);
         }
         $line = "====================================\n";
         file_put_contents($mixFile,$line,FILE_APPEND);
     }
+
+    //国家*性别*年龄
+    foreach($intersectionArr as $nation => $intersection){
+        $line = "[{$nation}]\n";
+        file_put_contents($intersectionFile,$line,FILE_APPEND);
+        
+        foreach($intersection as $gender => $ageArr){
+            $line = "-----------------------------------\n";
+            file_put_contents($intersectionFile,$line,FILE_APPEND);
+            $line = "(gender){$gender}\n";
+            file_put_contents($intersectionFile,$line,FILE_APPEND);
+            $line = "age\tnum\tconsumption\n";
+            file_put_contents($intersectionFile,$line,FILE_APPEND);
+            foreach($ageArr as $age => $one){
+                $one["consumption"] = round($one["consumption"]/$one["num"],2);
+                $line = implode("\t",$one)."\n";
+                file_put_contents($intersectionFile,$line,FILE_APPEND);
+            }
+        }       
+        $line = "====================================\n";
+        file_put_contents($intersectionFile,$line,FILE_APPEND);
+    }
+
